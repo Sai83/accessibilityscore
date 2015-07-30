587c730e6f44a04a58f4898ba8adee4c2ba0fa35
David <shuaixin.david@gmail.com>
2015-07-30 18:55:10 -0500

    new materils

--- Geolocation/parse_ctu.py
+++ Geolocation/parse_ctu.py
@@ -9,13 +9,35 @@ ctu_ref_file = "CTU_ref.csv"
 ctu = pd.read_csv(ctu_raw_file)
 #print(ctu)
 
-ctu['FEATURE_NAME'] = ctu['FEATURE_NAME'].apply(lambda x:x.split('of ')[-1].strip())
-ctu['COUNTY_NAME'] = ctu['COUNTY_NAME'].apply(lambda x:x.replace('County', '').strip())
+ctu_sel = []
+for index, row in ctu.iterrows():
+    feature = row['FEATURE_NAME']
+    type = row['CTU_TYPE']
+    lat = row['LATITUDE']
+    lng = row['LONGITUDE']
+    if type == 'City':
+        if '(historical)' not in feature:
+            city = feature.split('of ')[-1].strip()
+            city = city.replace('Saint', 'St')
+            ctu_sel.append({'name':city, 'type':'city', 'lat':lat, 'lng':lng})
+    elif type == 'County':
+        county = feature.replace('County', '').strip()
+        county = county.replace(' qui ', ' Qui ')
+        ctu_sel.append({'name':county, 'type':'county', 'lat':lat, 'lng':lng})
+    else:
+        continue
 
-
-print(ctu)
-ctu[['FEATURE_NAME', 'CTU_TYPE', 'LATITUDE',
-     'LONGITUDE', 'CENSUS_CODE', 'COUNTY_NAME']].to_csv(ctu_ref_file, sep='\t', index=False)
+ctu_sel = pd.DataFrame(ctu_sel)
+ctu_sel.drop_duplicates(inplace=True)
+ctu_sel.to_csv(ctu_ref_file, index=False, columns=['name', 'type', 'lat', 'lng'])
+        
+##ctu['FEATURE_NAME'] = ctu['FEATURE_NAME'].apply(lambda x:x.split('of ')[-1].strip())
+##ctu['COUNTY_NAME'] = ctu['COUNTY_NAME'].apply(lambda x:x.replace('County', '').strip())
+##
+##
+##print(ctu)
+##ctu[['FEATURE_NAME', 'CTU_TYPE', 'LATITUDE',
+##     'LONGITUDE', 'CENSUS_CODE']].to_csv(ctu_ref_file, sep='\t', index=False)
 
 ##chunksize = 2000
 ##mn_dfs = pd.DataFrame()
